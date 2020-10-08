841cbb807ac071c482cd870e001c92719913a5ec
magdyMohammed20 <magdymohammed37@yahoo.com>
2020-10-08 19:25:32 +0200

    Create Hero Section Spin Animation And Wave

--- src/Components/hero/Hero.js
+++ src/Components/hero/Hero.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import hero from '../../images/hero.png'
-
+import './hero.css'
 function Hero() {
     return (
-        <div className='bg-indigo-900 h-screen'>
-            <div className='container-xl flex px-6 h-full'>
-                <div className='text-white text-center w-full lg:w-1/2 lg:text-left lg:p-5 h-full flex flex-col justify-center'>
+        <div className='bg-indigo-900 h-screen relative'>
+            <div className='container-xl flex flex-wrap px-6 h-full'>
+                <div className='text-white text-center w-full lg:w-1/2 lg:text-left lg:p-5 flex flex-col justify-center'>
                     <span className='mb-5'>STUNNING TEMPLATE</span>
                     <h2 className='text-2xl lg:text-5xl font-bold leading-snug'>
                         Build Your Website with Jules UI Kit Template
@@ -21,7 +21,12 @@ function Hero() {
                 <div className='w-1/2 p-5 hidden lg:inline-block'>
                     <img src={hero} />
                 </div>
+                <div className='bg-white mx-auto rounded-full flex justify-center overflow-hidden' style={{ width: '30px', height: '50px' }}>
+                    <span className='hero-spin bg-indigo-900 rounded-full block mt-2'></span>
+                </div>
             </div>
+
+            <img src={'https://assets.website-files.com/5e09f044743608547fe6e12a/5e130c3e6a42a449fb63c612_decor-v1.svg'} className='absolute bottom-0 left-0 w-full' />
         </div>
     )
 }
